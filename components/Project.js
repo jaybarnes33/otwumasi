7c5bd55de2cd0ab1072ee5529762c19662a851f9
Oduro T.J Barnes <ohenesetwumasi@gmail.com>
2021-03-30 11:00:17 +0000

    Add projects

--- components/Project.js
+++ components/Project.js
@@ -2,12 +2,12 @@ import styles from "../styles/Home.module.css";
 const Project = ({ image, desc, demo }) => {
   return (
     <>
-      <div>
+      <div className={styles.project}>
         <img className={styles.projectImage} src={image} />
 
         <div className={styles.projectText}>
           <p>{desc}</p>
-          <a type="button" href={demo}>
+          <a className={styles.projectButton} href={demo}>
             Demo
           </a>
         </div>
