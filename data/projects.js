85fc446ecda308ef083390bdc0ab4fb1ac392339
Oduro T.J Barnes <ohenesetwumasi@gmail.com>
2021-03-30 10:34:17 +0000

    Add projects

---
+++ data/projects.js
@@ -0,0 +1,24 @@
+const projects = [
+  {
+    name: "Atrady",
+    desc: "Online Marketplace for students",
+    image: "/images/portfolio/atrady.png",
+  },
+  {
+    name: "Atrady",
+    desc: "Online Marketplace for students",
+    image: "/images/portfolio/atrady.png",
+  },
+  {
+    name: "Atrady",
+    desc: "Online Marketplace for students",
+    image: "/images/portfolio/atrady.png",
+  },
+  {
+    name: "Atrady",
+    desc: "Online Marketplace for students",
+    image: "/images/portfolio/atrady.png",
+  },
+];
+
+export default projects;
